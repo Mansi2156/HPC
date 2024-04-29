c3734898d2fc778f39ee01cf3705147d6cd56477
Mansi2156 <112949786+Mansi2156@users.noreply.github.com>
2024-04-29 19:51:13 +0530

    Update Merge.cpp

--- Merge.cpp
+++ Merge.cpp
@@ -3,85 +3,90 @@
 #include<omp.h>
 using namespace std;
 
-void mergesort(int a[], int i, int j);
-void merge(int a[], int i1, int j1, int i2,int j2);
+void mergesort(int a[],int i,int j);
+void merge(int a[],int i1,int j1,int i2,int j2);
 
-void mergesort(int a[], int i, int j)
+void mergesort(int a[],int i,int j)
 {
-  int mid;
-	if(i < j)
+	omp_set_num_threads(4);
+	int mid;
+	if(i<j)
 	{
-    	mid=(i+j)/2;
-    	#pragma omp parallel sections
-    	{
-        	#pragma omp section
-        	{
-            	mergesort(a,i,mid);   	 
-        	}
-        	#pragma omp section
-        	{
-            	mergesort(a,mid+1,j);    
-        	}
-    	}
-    	merge(a,i,mid,mid+1,j);    
+		//array is recursively divided into two halves
+    		mid=(i+j)/2;
+
+		//each half is sorted in parallel using OpenMP sections
+    		#pragma omp parallel sections
+    		{
+
+        			#pragma omp section
+        			{
+            				mergesort(a,i,mid);   	 
+        			}
+
+        			#pragma omp section
+        			{
+            				mergesort(a,mid+1,j);    
+        			}
+    		}
+
+    		merge(a,i,mid,mid+1,j);    
 	}
+
 }
  
-void merge(int a[], int i1, int j1, int i2, int j2)
+void merge(int a[],int i1,int j1,int i2,int j2)
 {
 	int temp[1000];    
 	int i,j,k;
 	i=i1;    
 	j=i2;    
-	k=0;
+	k=0; //index of temporary array
     
-	while(i <= j1 && j <= j2)    
+	while(i<=j1 && j<=j2)    
 	{
-    	if(a[i] < a[j])
-    	{
-        	temp[k++] = a[i++];
-    	}
-    	else
-    	{
-        	temp[k++] = a[j++];
-    }    
+    		if(a[i]<a[j])
+    		{
+        			temp[k++]=a[i++];
+    		}
+    		else
+    		{
+        			temp[k++]=a[j++];
+    		}    
 	}
     
-	while(i <= j1)    
+	while(i<=j1)    
 	{
-    	temp[k++] = a[i++];
+    		temp[k++]=a[i++];
 	}
    	 
-	while(j <= j2)    
+	while(j<=j2)    
 	{
-    	temp[k++] = a[j++];
+    		temp[k++]=a[j++];
 	}
    	 
-	for(i=i1,j=0; i<=j2; i++,j++)
+	for(i=i1,j=0;i<=j2;i++,j++)
 	{
-    	a[i]=temp[j];
+    		a[i]=temp[j];
 	}    
 }
 
 
 int main()
 {
-  /*
-	int *a,n,i;
-	cout<<"\n enter total no of elements=>";
-	cin>>n;
-	a= new int[n];
+	/*
+	int *a,n;
+    	cout<<"\n enter total no of elements=>";
+    	cin>>n;
+    	a=new int[n];
+    	cout<<"\n enter elements=>";
+    	for(int i=0;i<n;i++)
+    	{
+   	 	cin>>a[i];
+    	}
+	*/
 
-	cout<<"\n enter elements=>";
-	for(i=0;i<n;i++)
-	{
-    	cin>>a[i];
-	}
-
-	mergesort(a, 0, n-1);
-  */
-  
-  int SIZE =200;
+	int SIZE =200;
  	int i,j;
     	int a[SIZE];
     
@@ -91,7 +96,7 @@ int main()
     	}
     
    	int n = SIZE;
-    double start,end;	
+    	double start,end;	
 
 	start=omp_get_wtime();
 	mergesort(a, 0, n-1);
@@ -100,9 +105,9 @@ int main()
 	cout<<"\n sorted array is=>";
 	for(i=0;i<n;i++)
 	{
-    	cout<<"\n"<<a[i];
+    		cout<<a[i]<<”  ”;
 	}
-  
-  Cout<<"\n-------------------------\n Time Parallel= "<<(end-start);
+  	
+	Cout<<"\n-------------------------\n Time Parallel= "<<(end-start);
 	return 0;
 }
